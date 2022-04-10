a884269cd7a4bd39b2db78a67e010176178be3ed
Alghuti-Portfolio <103196346+Alghuti-Portfolio@users.noreply.github.com>
2022-04-09 21:34:01 -0500

    Update FinTech_Search.py

--- FinTech_Search.py
+++ FinTech_Search.py
@@ -132,50 +132,11 @@ st.sidebar.write(candidate_address)
 st.sidebar.markdown("## Total Wage in Ether")
 
 ################################################################################
+
 # Step 2: Sign and Execute a Payment Transaction
 
-# Complete the following steps:
+################################################################################
 
-# 1. Fintech Finder customers will select a fintech professional from the
-# application interface’s drop-down menu, and then input the amount of time for
-# which they’ll hire the worker. Code the application so that once a customer
-# completes these steps, the application will calculate the amount that the
-# worker will be paid in ether. To do so, complete the following steps:
-
-    # * Write the equation that calculates the candidate’s wage. This equation
-    #  should assess the candidate’s hourly rate from the candidate database
-    # (`candidate_database[person][3]`) and then multiply this hourly rate by
-    # the value of the `hours` variable. Save this calculation’s output as a
-    # variable named `wage`.
-
-    # * Write the `wage` variable to the Streamlit sidebar by
-    # using `st.sidebar.write`.
-
-# 2. Now that the application can calculate a candidate’s wage, write the code
-# that will allow a customer (you, in this case) to send an Ethereum blockchain
-# transaction that pays the hired candidate. To accomplish this, locate the
-# code that reads `if st.sidebar.button("Send Transaction")`. You’ll need to
-# add logic to this `if` statement that sends the appropriate information to
-# the `send_transaction` function (which you imported from the `FTcrypto_wallet`
-# script file). Inside the `if` statement, add the following functionality:
-
-    # * Call the `send_transaction()` function and pass it three parameters:
-        # - Your Ethereum `account` information. (Remember that this `account`
-        # instance was created when the `generate_account` function was called.)
-        #  From the `account` instance, the application will be able to access the
-        #  `account.address` information that is needed to populate the `from` data
-        # attribute in the raw transaction.
-        #- The `candidate_address` (which will be created and identified in the
-        # sidebar when a customer selects a candidate). This will populate the `to`
-        # data attribute in the raw transaction.
-        # - The `wage` value. This will be passed to the `toWei` function to
-        # determine the wei value of the payment in the raw transaction.
-
-    # * Save the transaction hash that the `send_transaction` function returns
-    # as a variable named `transaction_hash`, and have it display on the
-    # application’s web interface.
-
-##########################################
 # Step 2 - Part 1:
 # * Write the equation that calculates the candidate’s wage. This equation
 # should assess the candidate’s hourly rate from the candidate database
